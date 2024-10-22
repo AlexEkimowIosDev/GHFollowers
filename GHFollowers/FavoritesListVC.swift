4c87740e5ac539a36c889b407c46ad1382751bfd
Alexey Ekimow <Ekimow1994@yandex.ru>
2024-10-22 21:45:18 +0300

    App Navigation Setup

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Alexey Ekimow on 22.10.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Alexey Ekimow on 22.10.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
